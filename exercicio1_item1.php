095e1c4bff13cc385bffd52404a88bf5516bdae3
Luísa C. Pimentel dos Santos <101952537+luisacps@users.noreply.github.com>
2024-10-03 11:54:13 -0300

    Add files via upload

---
+++ exercicio1_item1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            exercicio 1 - item 1
+        </title>
+    </head>
+    <body>
+        <?php
+        
+            $imagem = "<img src='https://picsum.photos/500/600' />";
+
+            echo "$imagem";
+            print "<h2>Imagem da Semana<h2>";
+        ?>
+    </body>
+</html>
