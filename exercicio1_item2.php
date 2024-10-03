494b10433ebd853341ddcc74f4bca561281e8946
Luísa C. Pimentel dos Santos <101952537+luisacps@users.noreply.github.com>
2024-10-03 12:02:00 -0300

    Add files via upload

---
+++ exercicio1_item2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            exercicio 1 - item 2
+        </title>
+    </head>
+    <body>
+        <h2>URLs Absolutas<h2>
+        <?php
+            echo "<a href='https://www.w3schools.com/'>w3schools</a><br>";
+            echo "<a href='https://www.google.com/'>google</a><br>";
+        ?>
+
+        <h2>URLs Relativas<h2>
+        <?php
+            echo "<a href='teste1.html'>html images</a><br>";
+            echo "<a href='teste2.html'>css tutorial</a><br>";
+        ?>
+    </body>
+</html>
